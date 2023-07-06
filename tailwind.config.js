57955f12ed497ab9d88fb9b81f3483acbdb174d7
Josh Galbreath <joshsgalbreath@gmail.com>
2023-07-06 12:45:20 -0400

    chore: init

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,11 @@ module.exports = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        sans: ['var(--font-charcoal)'],
       },
     },
   },
-  plugins: [],
-}
+};
