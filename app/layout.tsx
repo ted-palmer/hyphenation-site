ce900e157092fd8bddaecad97abbc93f5a5de61c
Josh Galbreath <joshsgalbreath@gmail.com>
2023-07-06 21:59:55 -0400

    chore: roll back prettier version / incompatible with prettier-plugin-tailwindcss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
-import clsx from 'clsx'
-import { Martian_Mono } from 'next/font/google'
-import './globals.css'
+import { Martian_Mono } from 'next/font/google';
 
-const martianMono = Martian_Mono({ subsets: ['latin'] })
+import './globals.css';
+import clsx from 'clsx';
+
+const martianMono = Martian_Mono({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Hyphen Site',
   description: 'Mint a hyphen.',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={clsx(martianMono.className, 'bg-background')}>{children}</body>
+      <body className={clsx(martianMono.className, 'bg-background')}>
+        <div className={'mx-auto w-full max-w-[60rem] grow p-4 md:px-20 md:py-16'}>{children}</div>
+      </body>
     </html>
-  )
+  );
 }
