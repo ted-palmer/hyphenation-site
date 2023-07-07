ee644c8e671af1801028245e1a175050f55c5530
Josh Galbreath <joshsgalbreath@gmail.com>
2023-07-07 00:06:39 -0400

    chore: add variants to buttons + init impl. for party, zora, opensea

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,9 @@ import { useAccount } from 'wagmi';
 
 import AdoptAHiphen from '@/public/adopt-a-hyphen.svg';
 import Logo from '@/public/hyphen-logo.svg';
+import Opensea from '@/public/opensea.svg';
+import Party from '@/public/party-vs-party.svg';
+import Zora from '@/public/zora.svg';
 
 import Button from '@/components/button';
 
@@ -14,12 +17,27 @@ export default function Home() {
   const { address } = useAccount();
 
   return (
-    <main className="flex-col items-center justify-between p-5 sm:p-24">
+    <main className="flex-col items-center justify-between p-5">
       <div className="flex flex-col items-center space-y-8">
         <Image width={300} src={Logo} alt="Hyphen Logo" />
         <Image width={300} src={AdoptAHiphen} alt="Hyphen Logo" />
+        <div className="flex flex-col space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
+          <Button className="px-4" leftIcon={<Image alt="Party Logo" src={Party} />} color="white">
+            PARTY VS PARTY
+          </Button>
+          <Button className="px-4" leftIcon={<Image alt="Zora Logo" src={Zora} />} color="white">
+            MINT ON ZORA
+          </Button>
+          <Button
+            leftIcon={<Image alt="Opensea Logo" src={Opensea} />}
+            className="px-4"
+            color="white"
+          >
+            VIEW ON OPENSEA
+          </Button>
+        </div>
         <div className="flex flex-col items-center">
-          {!address ? <ConnectButton /> : <Button>Adopt a Hyphen</Button>}
+          {!address ? <ConnectButton /> : <Button color="white">Adopt a Hyphen</Button>}
           <div className="mt-4 text-center text-xs md:text-sm">
             Approve your adoption tickets to enable redemption.
           </div>
--- components/button.tsx
+++ components/button.tsx
@@ -1,11 +1,45 @@
-type ButtonProps = JSX.IntrinsicElements['button'];
+import { cva } from 'class-variance-authority';
+import { twMerge } from 'tailwind-merge';
 
-const Button = ({ children, ...props }: ButtonProps) => {
+type ButtonProps = JSX.IntrinsicElements['button'] & {
+  color?: 'blue' | 'white' | 'black';
+  leftIcon?: React.ReactNode;
+  className?: string;
+};
+
+const buttonVariants = cva(
+  [
+    'rounded-xl',
+    'border',
+    'border-transparent',
+    'px-8',
+    'py-3',
+    'focus:outline-none',
+    'focus:ring-2',
+    'focus:ring-offset-2',
+    'flex',
+    'items-center',
+    'justify-center',
+  ],
+  {
+    variants: {
+      color: {
+        blue: 'bg-background text-background hover:bg-blue-600 active:bg-blue-700',
+        white: 'bg-white text-black hover:bg-gray-100 active:bg-gray-200',
+        black: 'bg-black text-white hover:bg-gray-800 active:bg-gray-900',
+      },
+      size: {
+        sm: ['text-sm', 'px-6', 'py-2'],
+        lg: ['text-base', 'px-8', 'py-3'],
+      },
+    },
+  },
+);
+
+const Button = ({ children, color = 'black', leftIcon, className, ...props }: ButtonProps) => {
   return (
-    <button
-      className="transition-color rounded-xl border border-transparent bg-black px-8 py-3 text-background hover:border-gray-500 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 active:bg-gray-900"
-      {...props}
-    >
+    <button className={twMerge(buttonVariants({ color, size: 'lg' }), className)} {...props}>
+      {leftIcon ? <span className="mr-2">{leftIcon}</span> : null}
       {children}
     </button>
   );
