ae44eeb23ecf5dae9726dc9e0cb488db2f23c735
Josh Galbreath <joshsgalbreath@gmail.com>
2023-07-08 16:09:29 -0400

    fix: webpack config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
   },
